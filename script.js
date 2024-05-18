2c4ddf5536cfdb8455b6ce493a4fed3756e7e7fb
muskan36 <132078420+muskan36@users.noreply.github.com>
2024-05-18 22:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+burger= document.querySelector('.burger')
+nav =document.querySelector('.nav')
+list = document.querySelector('.list')
+
+burger.addEventListener('click',()=>{
+    list.classList.toggle('resp');
+   nav.classList('h-nav');
+})
